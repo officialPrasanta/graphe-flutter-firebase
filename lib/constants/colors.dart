c86ff07dbc59852ff8503bc149994211d71fd686
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-22 23:13:38 +0530

    new colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color kScaffolBackgroundColor = Color(0xFFCED9E7);
+const Color kTextFieldBorderColor = Color(0xFFB3B3B3);
+const Color kPrimaryButtonColor = Color(0xFF5277BB);
+const Color kTextColor = Color(0xFF121212);
+const Color kLabelColor = Color(0xFF121212);
