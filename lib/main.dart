bfc3c390603ce63e80f23cfe999384bca1e86681
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-22 21:59:48 +0530

    firebase initilization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:graphe_firebase_notification/firebase_options.dart';
 import 'package:graphe_firebase_notification/view/login.dart';
 
-void main() {
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
