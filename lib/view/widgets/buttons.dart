a638b7e8db17a28b2b5e3c732d1f1fc409e0331b
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-23 03:33:25 +0530

    add prefix imageand custom bg and borderRadius

--- lib/view/widgets/buttons.dart
+++ lib/view/widgets/buttons.dart
@@ -4,18 +4,25 @@ import 'package:graphe_firebase_notification/constants/colors.dart';
 Widget primaryButton(
     {required BuildContext context,
     required String labelText,
-    void Function()? onTap}) {
+    void Function()? onTap,
+    double? borderRadius,
+    String? prefixImagePath}) {
   return InkWell(
     onTap: onTap,
     child: Ink(
       decoration: BoxDecoration(
-        borderRadius: BorderRadiusDirectional.circular(5),
+        borderRadius: BorderRadiusDirectional.circular(borderRadius ?? 5),
         color: kPrimaryButtonColor,
       ),
       padding: const EdgeInsets.symmetric(vertical: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          prefixImagePath != null
+              ? ClipOval(
+                  child: Image.asset(prefixImagePath),
+                )
+              : const SizedBox(),
           Text(
             labelText,
             style: Theme.of(context).textTheme.headlineSmall,
