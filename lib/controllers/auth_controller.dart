de07343d95cd0a5fcaa02cbcfccd9902f4198347
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-23 06:48:55 +0530

    update date picker

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:graphe_firebase_notification/constants/dateformat.dart';
 
 class AuthController extends GetxController {
-  Rx<DateTime> selectedDate = DateTime.now().obs;
+  DateTime now = DateTime.now();
+  Rx<String> selectedDate = ''.obs;
   DateTime? pickedDate;
   void openDatePicker(BuildContext context) async {
     pickedDate = await showDatePicker(
         context: context,
-        initialDate: selectedDate.value,
+        initialDate: now,
         firstDate: DateTime(1900),
         lastDate: DateTime(2100));
-    if (pickedDate != null && pickedDate != selectedDate.value) {
-      selectedDate.value = pickedDate!;
+    if (pickedDate != null && pickedDate!.isBefore(now)) {
+      if ((now.difference(pickedDate!)..inDays) > const Duration(days: 6574)) {
+        selectedDate.value = ddMMYYYYFormat.format(pickedDate!);
+      } else {
+        Fluttertoast.showToast(msg: 'You must be 18 years old');
+      }
     }
   }
 }
