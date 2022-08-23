83c6300b6df8e5ec033502be4bed4998e8aca345
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-23 06:04:54 +0530

    datepicker function

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController{
-  
+class AuthController extends GetxController {
+  Rx<DateTime> selectedDate = DateTime.now().obs;
+  DateTime? pickedDate;
+  void openDatePicker(BuildContext context) async {
+    pickedDate = await showDatePicker(
+        context: context,
+        initialDate: selectedDate.value,
+        firstDate: DateTime(1900),
+        lastDate: DateTime(2100));
+    if (pickedDate != null && pickedDate != selectedDate.value) {
+      selectedDate.value = pickedDate!;
+    }
+  }
 }
