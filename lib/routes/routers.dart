a655d51977d808bc56691d68c49c1138e7575795
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-25 02:31:07 +0530

    router services

--- lib/routes/routers.dart
+++ lib/routes/routers.dart
@@ -1,7 +1,13 @@
+import 'package:get/get.dart';
+import 'package:graphe_firebase_notification/service/root.dart';
+import 'package:graphe_firebase_notification/view/home.dart';
+import 'package:graphe_firebase_notification/view/login.dart';
+import 'package:graphe_firebase_notification/view/register.dart';
+
 List<GetPage> router = [
-        GetPage(name: '/ROOT', page: () => Root()),
-        GetPage(name: '/LOGIN', page: () => Login()),
-        GetPage(name: '/HOME', page: () => Home()),
-        GetPage(name: '/DETAILS_USER', page: () => RegisterUser()),
+        GetPage(name: '/ROOT', page: () => const Root()),
+        GetPage(name: '/LOGIN', page: () => const Login()),
+        GetPage(name: '/HOME', page: () => const Home()),
+        GetPage(name: '/REGISTER_USER', page: () => const RegisterUser()),
         // GetPage(name: 'SIGNUP', page: ()=>SignUp()),
       ];
