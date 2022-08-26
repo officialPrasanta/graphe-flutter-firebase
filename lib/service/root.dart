b37999a7875691a76e37025f416a041699b3b46f
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-26 11:59:29 +0530

    root updated

--- lib/service/root.dart
+++ lib/service/root.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:graphe_firebase_notification/constants/measurement.dart';
 import 'package:graphe_firebase_notification/controllers/auth_controller.dart';
-import 'package:graphe_firebase_notification/view/widgets/buttons.dart';
+import 'package:graphe_firebase_notification/model/usermodel.dart';
+import 'package:graphe_firebase_notification/view/home.dart';
+import 'package:graphe_firebase_notification/view/login.dart';
+import 'package:graphe_firebase_notification/view/register.dart';
 
 class Root extends StatelessWidget {
   const Root({Key? key}) : super(key: key);
@@ -12,25 +15,25 @@ class Root extends StatelessWidget {
     AuthController ac = Get.find<AuthController>();
     final User? user = ac.auth.currentUser;
     return user == null
-        ? SignUp()
+        ? const Login()
         : FutureBuilder<UserData?>(
             future: ac.fetchUserData(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
                 //dynamic routing
                 if (snapshot.hasError) {
-                  return Login();
+                  return const Login();
                 } else {
                   ac.userData.value = snapshot.data!;
-                  if (ac.userData.value.name!.isEmpty) {
-                    return UserDetails();
+                  if (ac.userData.value.fullName!.isEmpty) {
+                    return const RegisterUser();
                   }else{
-                    return Home();
+                    return const Home();
                   }
                 }
               } else {
                 //splash screen
-                return Scaffold(
+                return const Scaffold(
                     body: Center(child: CircularProgressIndicator()));
               }
             });
