5bca4b06c7f05f4c40bdc3afbb9896c4321898c6
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-27 22:51:24 +0530

    update home screen

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:graphe_firebase_notification/constants/measurement.dart';
 import 'package:graphe_firebase_notification/controllers/auth_controller.dart';
+import 'package:graphe_firebase_notification/view/widgets/buttons.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,22 +11,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     AuthController ac = Get.find<AuthController>();
     return Scaffold(
+      bottomNavigationBar: primaryButton(
+        context: context,
+        labelText: 'All Notification',
+      ),
       appBar: AppBar(
-          title: Text(
-        'Home',
-        style: Theme.of(context).textTheme.titleSmall,
-      )),
+        title: Text(
+          'Home',
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+      ),
       body: Container(
         margin: const EdgeInsets.all(kMargin),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                'Hi, ${ac.userData.value.fullName?.split(' ')[0] ?? 'visitors'}!',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            ],
-          ),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Hi, ${ac.userData.value.fullName?.split(' ')[0] ?? 'visitors'}!',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
