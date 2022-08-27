1b479c31224e5936534431ff85f2ab394554b7c8
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-27 22:50:36 +0530

    add themedata

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/route_manager.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:graphe_firebase_notification/config/instance_binding.dart';
-import 'package:graphe_firebase_notification/constants/colors.dart';
 import 'package:graphe_firebase_notification/firebase_options.dart';
 import 'package:graphe_firebase_notification/routes/routers.dart';
+import 'package:graphe_firebase_notification/theme/theme_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,47 +30,7 @@ class MyApp extends StatelessWidget {
     ]);
     return GetMaterialApp(
       title: 'Firebase Notifications',
-      theme: ThemeData(
-        scaffoldBackgroundColor: kScaffolBackgroundColor,
-        textTheme: TextTheme(
-          titleLarge: GoogleFonts.openSans(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            height: 1.2,
-            color: kTextColor,
-          ),
-          titleMedium: GoogleFonts.openSans(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            height: 1.2,
-            color: kLabelColor,
-          ),
-          titleSmall: GoogleFonts.openSans(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            height: 1.2,
-            color: kOnPrimaryTextColor,
-          ),
-          bodyMedium: GoogleFonts.openSans(
-            fontSize: 14,
-            height: 1.2,
-            color: kTextColor,
-          ),
-          bodySmall: GoogleFonts.openSans(
-            fontSize: 12,
-            color: kTextColor,
-          ),
-          labelSmall: GoogleFonts.openSans(
-            fontSize: 14,
-            color: kLabelColor,
-          ),
-          headlineSmall: GoogleFonts.openSans(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: kTextColor,
-          ),
-        ),
-      ),
+      theme: themeData,
       builder: EasyLoading.init(),
       initialBinding: InstanceBinding(),
       getPages: router,
