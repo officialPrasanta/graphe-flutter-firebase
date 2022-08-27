fce84e8cfc2680afbf3e66e74a3ae632815c7094
officialPrasanta <prasanta.at.coding@gmail.com>
2022-08-27 22:45:09 +0530

    primary color and swatch map

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,8 +2,23 @@ import 'package:flutter/material.dart';
 
 const Color kScaffolBackgroundColor = Color(0xFFCED9E7);
 const Color kBorderColor = Color(0xFFB3B3B3);
-const Color kPrimaryButtonColor = Color(0xFF638FE0);
 const Color kTextColor = Color(0xFF121212);
 const Color kLabelColor = Color(0xFF2B2B2B);
 const Color kButtonTextColor = Color(0xFFF2F2F2);
 const Color kOnPrimaryTextColor = Color(0xFFF2F2F2);
+
+//primary color
+const int kPrimaryColor = 0xFF638FE0;
+//swatch
+Map<int, Color> kPrimaryColorSwatch = {
+  50: const Color(0xFFa1bcec),
+  100: const Color(0xFF92b1e9),
+  200: const Color(0xFF82a5e6),
+  300: const Color(0xFF739ae3),
+  400: const Color(0xFF638FE0),
+  500: const Color(0xFF638fe0),
+  600: const Color(0xFF5981ca),
+  700: const Color(0xFF4f72b3),
+  800: const Color(0xFF45649d),
+  900: const Color(0xFF3b5686),
+};
